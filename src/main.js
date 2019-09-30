af43a495f75f476dcadd968790a1c0bd687ba6ad
251205668@qq.com <251205668@qq.com>
2019-09-30 22:47:45 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css'
+// 引入reset.css使得移动端默认样式相同
+import './assets/styles/border.css'
+// 引入一像素边框的解决方案 可能存在2倍屏
+import fastClick from 'fastclick'
+// 解决点击300ms延迟问题
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+fastClick.attach(document.body)
+    /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>'
 })
