0f2225e792580062c7b04a322517f711ecd72dc0
251205668@qq.com <251205668@qq.com>
2019-10-03 22:20:48 +0800

    tijiao

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,62 +1,91 @@
 <template>
-  <div class="icons">
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-             alt="">
-      </div>
-      <p>景点门票</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png"
-             alt="">
-      </div>
+  <div>
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <div class="icons">
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+                   alt="">
+            </div>
+            <p>景点门票</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png"
+                   alt="">
+            </div>
 
-      <p>动植物园</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/3ef092d0a89ffddb7f781cda30c8ae49.png"
-             alt="">
-      </div>
-      <p>国庆大促</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/f5/c4c9d9830404e602.png"
-             alt="">
-      </div>
-      <p>室内娱乐</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png"
-             alt="">
-      </div>
-      <p>一日游</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png"
-             alt="">
-      </div>
-      <p>黄浦江游</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png"
-             alt="">
-      </div>
-      <p>上海野生</p>
-    </div>
-    <div class="icon1">
-      <div class="img-div">
-        <img src="http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png"
-             alt="">
-      </div>
-      <p>上海迪士尼</p>
-    </div>
+            <p>动植物园</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/3ef092d0a89ffddb7f781cda30c8ae49.png"
+                   alt="">
+            </div>
+            <p>国庆大促</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/f5/c4c9d9830404e602.png"
+                   alt="">
+            </div>
+            <p>室内娱乐</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png"
+                   alt="">
+            </div>
+            <p>一日游</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png"
+                   alt="">
+            </div>
+            <p>黄浦江游</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png"
+                   alt="">
+            </div>
+            <p>上海野生</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png"
+                   alt="">
+            </div>
+            <p>上海迪士尼</p>
+          </div>
+        </div>
+
+      </swiper-slide>
+
+      <swiper-slide>
+        <div class="icons">
+          <div class="icon1">
+            <div class="img-div">
+              <img src="https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png"
+                   alt="">
+            </div>
+            <p>上海海关</p>
+          </div>
+          <div class="icon1">
+            <div class="img-div">
+              <img src="http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png"
+                   alt="">
+            </div>
+
+            <p>城市观光</p>
+          </div>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"
+           slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
@@ -67,6 +96,12 @@ export default {
   props: [''],
   data () {
     return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        // loop: true,
+        // autoplay: 3000,
+        // autoplayDisableOnInteraction: false
+      }
 
     };
   },
@@ -123,6 +158,9 @@ p {
   height: 0.44rem;
   text-align: center;
   columns: #333333;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 
 img {
